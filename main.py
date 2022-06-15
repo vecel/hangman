00f64fa5e476ed8f3efda4d853284e0c894ae190
Mateusz Karandys <mateusz.karandys@gmail.com>
2022-06-15 21:48:17 +0200

    Add game logic and user input

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def definition(word_object: dict) -> str :
     return word_object['definition']
 
 def find_all(base_string: str, sub: str) -> list :
+    ''' Find and return list of all occurences of sub in base_string '''
+
     indexes = []
     start = 0
     while start < len(base_string):
@@ -24,8 +26,10 @@ def find_all(base_string: str, sub: str) -> list :
     return indexes
 
 def match(current: str, answer: str, letter: str) -> str :
-    if letter not in answer:
-        return current
+    ''' Fill the "current" word status with letter where it occurs in answer 
+    
+        Return updated word status '''
+    
     occurences = find_all(answer, letter)
     new_current = ''
     last_idx = 0
@@ -35,6 +39,45 @@ def match(current: str, answer: str, letter: str) -> str :
     new_current += current[last_idx:]
     return new_current
 
-WORD_OBJECT = get_word_object()
-answer = word(WORD_OBJECT)
-answer_definition = definition(WORD_OBJECT)
+def take_letter() -> str :
+    ''' Ask user for a letter and check if it is correct (single letter, a-z or A-Z)
+        
+        Return that letter if it is correct "#" otherwise'''
+
+    char = input('Type a character: ')
+    if len(char) == 1 and char.isalpha() :
+        return char.lower()
+    return '#'
+
+def empty_word(string: str) -> str :
+    ''' Construct string composed of "_" characters
+    
+        Return string that's length is same as given string's length '''
+
+    empty = ''
+    for i in string:
+        empty += '_'
+    return empty 
+
+def run() -> None :
+    WORD_OBJECT = get_word_object()
+    answer = word(WORD_OBJECT)
+    answer_definition = definition(WORD_OBJECT)
+    lifes = 10
+
+    word_status = empty_word(answer)
+    print(word_status)
+    while lifes > 0:
+        guess = take_letter()
+        if guess == '#': 
+            print('Wrong input, try again')
+            continue
+        if guess not in answer:
+            lifes -= 1
+            print('No', guess, 'in answer, remaining lifes: ', lifes)
+            continue
+
+        word_status = match(word_status, answer, guess)
+        print(word_status)
+
+run()
